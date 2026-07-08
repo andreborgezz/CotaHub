5089baded759d30ee18f63f9ad3cc321108a6fc2
andreborgezz <andre06borges06@gmail.com>
2026-07-08 16:01:09 -0300

    criando rotas para euro e pesos: argentino, chileno e uruguaio

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,14 +29,89 @@ app.get('/api/cotacoes/dolar', async(req,res) => {
         "compra": resposta.data.compra,
         "venda": resposta.data.venda,
         "fechoAnterior": resposta.data.fechoAnterior,
-        "dataAtualizacao": resposta.data.dataAtualizacao,
+        "dataAtualizacao": new Date(resposta.data.dataAtualizacao).toLocaleString('pt-BR', {timeZone: 'America/Sao_Paulo'}) 
+        //formatando a data pra voltar como: "08/07/2026, 11:02:00"
         });
     } catch(erro) {
-        res.status(500).json({erro: 'Erro ao consultar cotação: USD'})
+        res.status(500).json({erro: 'Erro ao consultar cotação: Dolar $'})
 }
 });
 
+// rota EURO: 
+app.get('/api/cotacoes/euro', async(req,res) => {
+    try{
+        const resposta = await axios.get('https://br.dolarapi.com/v1/cotacoes/eur');
 
+        res.json({
+        "moeda": resposta.data.moeda,
+        "nome": resposta.data.nome,
+        "compra": resposta.data.compra,
+        "venda": resposta.data.venda,
+        "fechoAnterior": resposta.data.fechoAnterior,
+        "dataAtualizacao": new Date(resposta.data.dataAtualizacao).toLocaleString('pt-BR', {timeZone: 'America/Sao_Paulo'}) 
+        //formatando a data pra voltar como: "08/07/2026, 11:02:00"
+        });
+    } catch(erro) {
+        res.status(500).json({erro: 'Erro ao consultar cotação: Euro €'})
+}
+});
+
+// rota PESO ARGENTINO: 
+app.get('/api/cotacoes/argentino', async(req,res) => {
+    try{
+        const resposta = await axios.get('https://br.dolarapi.com/v1/cotacoes/ars');
+
+        res.json({
+        "moeda": resposta.data.moeda,
+        "nome": 'Peso Argentino',
+        "compra": resposta.data.compra,
+        "venda": resposta.data.venda,
+        "fechoAnterior": resposta.data.fechoAnterior,
+        "dataAtualizacao": new Date(resposta.data.dataAtualizacao).toLocaleString('pt-BR', {timeZone: 'America/Sao_Paulo'}) 
+        //formatando a data pra voltar como: "08/07/2026, 11:02:00"
+        });
+    } catch(erro) {
+        res.status(500).json({erro: 'Erro ao consultar cotação: Peso Argentino $'})
+}
+});
+
+// rota PESO CHILENO: 
+app.get('/api/cotacoes/chileno', async(req,res) => {
+    try{
+        const resposta = await axios.get('https://br.dolarapi.com/v1/cotacoes/clp');
+
+        res.json({
+        "moeda": resposta.data.moeda,
+        "nome": 'Peso Chileno',
+        "compra": resposta.data.compra,
+        "venda": resposta.data.venda,
+        "fechoAnterior": resposta.data.fechoAnterior,
+        "dataAtualizacao": new Date(resposta.data.dataAtualizacao).toLocaleString('pt-BR', {timeZone: 'America/Sao_Paulo'}) 
+        //formatando a data pra voltar como: "08/07/2026, 11:02:00"
+        });
+    } catch(erro) {
+        res.status(500).json({erro: 'Erro ao consultar cotação: Peso Chileno $'})
+}
+});
+
+// rota PESO URUGUAIO: 
+app.get('/api/cotacoes/uruguaio', async(req,res) => {
+    try{
+        const resposta = await axios.get('https://br.dolarapi.com/v1/cotacoes/uyu');
+
+        res.json({
+        "moeda": resposta.data.moeda,
+        "nome": 'Peso Uruguaio',
+        "compra": resposta.data.compra,
+        "venda": resposta.data.venda,
+        "fechoAnterior": resposta.data.fechoAnterior,
+        "dataAtualizacao": new Date(resposta.data.dataAtualizacao).toLocaleString('pt-BR', {timeZone: 'America/Sao_Paulo'}) 
+        //formatando a data pra voltar como: "08/07/2026, 11:02:00"
+        });
+    } catch(erro) {
+        res.status(500).json({erro: 'Erro ao consultar cotação: Peso Uruguaio $'})
+}
+});
 app.listen(3000, () => {
     console.log("backend rodando na porta 3000");
     
